6b271dde8d858b06934a43d15e650d59762a67ac
焦方浩 <jiaofanghao@gzads.com>
2019-11-13 15:51:19 +0800

    [Init] Init The Project

---
+++ src/main/java/com/project/debug/App.java
@@ -0,0 +1,13 @@
+package com.project.debug;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
